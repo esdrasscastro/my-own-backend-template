3eb15c4f200b3f15f10fce3a652f843b3bd1f232
Esdras Castro <esdras.castro@accenture.com>
2021-04-08 14:10:37 -0300

    inclui biblioteca para ver o tamanho em bytes de um objeto. Além disso, foi adicionado uma pasta para colocar os arquivos que serão salvos futuramente na pasta do mockserver

--- api/modules/m/controller.js
+++ api/modules/m/controller.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const { LogMessage } = require('oi-log-message');
+const ObjectByteSize = require('../../util/ObjectByteSize');
 
 module.exports = class ListController {
     constructor() {
@@ -7,6 +9,16 @@ module.exports = class ListController {
 
     async listAll(req, res, send) {
         this.log.setRequest(req);
+        const obj = {
+            teste: 'object'
+        }
+
+        const { size } = new ObjectByteSize(obj);
+
+        const buffer = new Buffer.from(JSON.stringify(obj), 'utf-8');
+
+        fs.writeFileSync('./__files/teste.json', buffer);
+
         this.log.success('Sucesso na listagem dos dados');
         res.send(200);
     }
---
+++ api/util/ObjectByteSize.js
@@ -0,0 +1,41 @@
+module.exports =
+    /**
+     * @param {object} Objeto a ser retornado o tamanho 
+     * @returns O tamanho do objeto em Bytes
+     */
+    class ObjectByteSize {
+        constructor(object) {
+            this.size = this.roughSizeOfObject(object);
+        }
+        roughSizeOfObject(object) {
+            var objectList = [];
+            var stack = [object];
+            var bytes = 0;
+
+            while (stack.length) {
+                var value = stack.pop();
+
+                if (typeof value === 'boolean') {
+                    bytes += 4;
+                }
+                else if (typeof value === 'string') {
+                    bytes += value.length * 2;
+                }
+                else if (typeof value === 'number') {
+                    bytes += 8;
+                }
+                else if
+                    (
+                    typeof value === 'object'
+                    && objectList.indexOf(value) === -1
+                ) {
+                    objectList.push(value);
+
+                    for (var i in value) {
+                        stack.push(value[i]);
+                    }
+                }
+            }
+            return bytes;
+        }
+    }
